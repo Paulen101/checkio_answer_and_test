f6d0429a68d47cdb45f94824ade14916423d888b
Paulen101 <2023225chhun@aupp.edu.kh>
2023-08-24 13:02:17 +0700

    updated

---
+++ test.py
@@ -0,0 +1,20 @@
+a = [1, 2, 3]
+b = a
+a.append(4) # b is also changed
+# if a == b: 
+#     print("a and b are equal")
+# else:
+#     print("a and b are not equal")
+
+# print(b)
+
+# s1 = "hello world"
+# s2 = s1
+# s1 = s1.replace("hello world", "python")
+
+# print(s2)
+
+a = [1, 2, 3]
+# print(*a, sep = "\n")
+
+print(a[4])
