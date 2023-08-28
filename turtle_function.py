60aa9528cedbb81cfe8c834e7d9b4aee1c069ab4
Paulen101 <2023225chhun@aupp.edu.kh>
2023-08-28 19:36:44 +0700

    Create turtle_function.py

---
+++ turtle_function.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.shape('triangle')    
+turtle.color('red')
+turtle.pensize(5)
+turtle.speed(1)
+turtle.begin_fill()
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.color('blue')
+turtle.end_fill()
+turtle.exitonclick()
+
