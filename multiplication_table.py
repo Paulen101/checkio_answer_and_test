d2c42003ff157a2b9e4ba5948bd48bf766fc0b28
Paulen101 <2023225chhun@aupp.edu.kh>
2023-09-07 13:19:19 +0700

    updated

---
+++ multiplication_table.py
@@ -0,0 +1,16 @@
+# print("multiplication table")
+# print("  |", end = " ")
+# for j in range(1, 10): 
+#     print("  ", j, end = " ")
+# print()
+# print("-----------------------------------------")
+# for i in range(1, 10):
+#     print(i, "|", end = " ")
+#     for j in range(1, 10):
+#         print(format(i*j, "4d"), end = " ")
+#     print()
+    
+a = 1
+x = 2
+a = x ++1
+print(a)
