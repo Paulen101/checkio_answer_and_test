1b39962da4dcea7c2372021432a69106450cf68c
Paulen101 <2023225chhun@aupp.edu.kh>
2023-09-17 15:21:03 +0700

    updated

---
+++ main_test.py
@@ -0,0 +1,16 @@
+# balance = 1000 
+# while True: 
+#     if balance < 9: 
+#         break
+#     balance = balance -9 
+
+# print("balance is ", balance)
+
+# number = 0
+# sum = 0
+# for count in range(5):
+#      number = eval(input("Enter an integer: "))
+#      sum += number
+# print("sum is", sum)
+# print("count is", count)
+
--- multiplication_table.py
+++ multiplication_table.py
@@ -5,7 +5,13 @@ for j in range(1, 10):
 print()
 print("----------------------------------------------------")
 for i in range(1, 10):
-    print(i, "|", end = " ")
+    if i % 2 == 0:
+        print(i, "|", end = " ")
     for j in range(1, 10):
-        print(format(i*j, "4d"), end = " ")
+        if i % 2 == 0 and j % 2 == 0:
+            print(format(i*j, "4d"), end = " ")
     print()
+
+
+
+   
